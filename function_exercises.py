4704938065c9c5058ba077bae3165a707ef252e9
JeremyLagunas <jeremy.lagunas@gmail.com>
2022-06-02 10:32:13 -0500

    final draft function exercises

--- function_exercises.py
+++ function_exercises.py
@@ -1,90 +1,103 @@
 ## Define a function named is_two. It should accept one input and return True if the passed input is either the number or the string 2, False otherwise.
 def is_two(n):
-    if n in [2, 'two']:
+    if n in [2, '2']:
         print(True)
     else:
         print(False)
     return
-is_two('two')
+is_two('2')
 
 ## 2. Define a function named is_vowel. It should return True if the passed string is a vowel, False otherwise.
 def is_vowel(i):
-    if i.lower() in ['a', 'e', 'i', 'o', 'u']:
-        print(True)
-    else:
-        print(False)
-    return
+    return len(i) == 1 and i.lower() in 'aeiou'
 
-is_vowel('F')
+is_vowel('U')
 
 ## 3. Define a function named is_consonant. It should return True if the passed string is a consonant, False otherwise. Use your is_vowel function to accomplish this.
 def is_consonant(x):
-    if x.lower() in ['a', 'e', 'i', 'o', 'u']:
-        print(False)
-    else:
-        print(True)
+    return len(x) == 1 and x.isalpha() and not is_vowel(x)
+    
 
 is_consonant('X')
 
 ## 4. Define a function that accepts a string that is a word. The function should capitalize the first letter of the word if the word starts with a consonant.
-def capitalize_function(word):
-    if word[0] in ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']:
-        print(word.capitalize())
-    else:
-        print(word)
-    return
+def capitalize_starting_consonant(word):
+    if type(word) != str:
+        return False
+    first_letter = word[0]
+    if is_consonant(first_letter):
+        word = word.capitalize()
+    return word
 
-capitalize_function('apple')
+capitalize_function('banana')
 
 ## 5. Define a function named calculate_tip. It should accept a tip percentage (a number between 0 and 1) and the bill total, and return the amount to tip.
-tip_percentage = .20
-bill_total = 79.99
-def calculate_tip(bill_total):
-    tip_amount = tip_percentage * bill_total
+
+def calculate_tip(tip_percentage, bill):
+    tip_amount = tip_percentage * bill
     return tip_amount
 
-calculate_tip(bill_total)
+calculate_tip(.2, 50)
 
 ## 6. Define a function named apply_discount. It should accept a original price, and a discount percentage, and return the price after the discount is applied.
 discount_rate = .20
 original_price = 89.99
 
-def apply_discount(original_price):
-    discounted_price = original_price - (original_price * discount_rate)
+def apply_discount(discount_percentage, original_price):
+    discounted_price = original_price - (original_price * discount_percentage)
     return discounted_price
 
-apply_discount(original_price)
+apply_discount(.75, 50)
 
 ## 7. Define a function named handle_commas. It should accept a string that is a number that contains commas in it as input, and return a number as output.
 
 
 ## 8. Define a function named get_letter_grade. It should accept a number and return the letter grade associated with that number (A-F).
-def get_letter_grade(x):
-    if x in range(90, 101):
-        print('A')
-    elif x in range(80, 90):
-        print('B')
-    elif x in range(70, 80):
-        print('C')
+def get_letter_grade(grade):
+    if type(grade) == int or type(grade) == float:
+            if grade >= 90:
+                return 'A'
+            elif grade >= 80:
+                return 'B'
+            elif grade >= 70:
+                return 'C'
+            elif grade >= 60:
+                return 'D'
+            else:
+                return 'F'
     else:
-        print('F')
-    return
-
-get_letter_grade(90)
+        return 'Input must be a number'
+get_letter_grade('A')
 
 ## 9. Define a function named remove_vowels that accepts a string and returns a string with all the vowels removed.
-vowels = ['a', 'e', 'i', 'o', 'u']
-def remove_vowels(word):
-    result = [i for i in word if i.lower() not in vowels]
+def remove_vowels(string):
+    vowels = 'aeiou'
+    result = [i for i in string if i.lower() not in vowels]
     result = ''.join(result)
-    print(result)
-    return
+    return result
 
-remove_vowels('aeiouy')
+remove_vowels('yayeYIyoyu')
 
 ## 10. Define a function named normalize_name. It should accept a string and return a valid python identifier, that is: anything that is not a valid python identifier should be removed, leading and trailing whitespace should be removed,everything should be lowercase,spaces should be replaced with underscores
+def remove_special_char(string):
+    return ''.join([c for c in string if c.isalnum() or c == ' '])
 
+remove_special_char('he!!0 w0rld!')
 
+def normalize_name(string):
+    special_char_removed = remove_special_char(string)
+    return special_char_removed.lower().strip().replace(' ', '_')
+
+normalize_name('apple pie is delicious')
 
 ## 11. Write a function named cumulative_sum that accepts a list of numbers and returns a list that is the cumulative sum of the numbers in the list.
 
+def cumulative_sum(nums):
+    output = []
+    total = 0
+    for num  in nums:
+        total += num
+        output.append(total)
+    return output
+
+cumulative_sum([1, 2, 3, 4, 5])
